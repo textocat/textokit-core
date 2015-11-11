20cd90c0316360374bf0e53bf32c39bcb37ec833
Rinat Gareev <rinat@textocat.com>
2015-11-11 20:23:35 +0300

    added copyright notice for source files of Textokit.Commons

--- Textokit.Commons/src/main/jcas/com/textocat/textokit/commons/DocumentMetadata.java
+++ Textokit.Commons/src/main/jcas/com/textocat/textokit/commons/DocumentMetadata.java
@@ -1,167 +1,183 @@
 
 
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 /* First created by JCasGen Wed Nov 11 19:41:34 MSK 2015 */
 package com.textocat.textokit.commons;
 
-import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JCasRegistry;
 import org.apache.uima.jcas.cas.TOP_Type;
 
 import org.apache.uima.jcas.tcas.Annotation;
 
 
-/** 
+/**
  * Updated by JCasGen Wed Nov 11 19:41:34 MSK 2015
  * XML source: src/main/resources/com/textocat/textokit/commons/Commons-TypeSystem.xml
  * @generated */
 public class DocumentMetadata extends Annotation {
   /** @generated
-   * @ordered 
+   * @ordered
    */
   @SuppressWarnings ("hiding")
   public final static int typeIndexID = JCasRegistry.register(DocumentMetadata.class);
   /** @generated
-   * @ordered 
+   * @ordered
    */
   @SuppressWarnings ("hiding")
   public final static int type = typeIndexID;
   /** @generated
-   * @return index of the type  
+   * @return index of the type
    */
   @Override
   public              int getTypeIndexID() {return typeIndexID;}
- 
+
   /** Never called.  Disable default constructor
    * @generated */
   protected DocumentMetadata() {/* intentionally empty block */}
-    
-  /** Internal - constructor used by generator 
+
+  /** Internal - constructor used by generator
    * @generated
    * @param addr low level Feature Structure reference
-   * @param type the type of this Feature Structure 
+   * @param type the type of this Feature Structure
    */
   public DocumentMetadata(int addr, TOP_Type type) {
     super(addr, type);
     readObject();
   }
-  
+
   /** @generated
-   * @param jcas JCas to which this Feature Structure belongs 
+   * @param jcas JCas to which this Feature Structure belongs
    */
   public DocumentMetadata(JCas jcas) {
     super(jcas);
-    readObject();   
-  } 
+    readObject();
+  }
 
   /** @generated
    * @param jcas JCas to which this Feature Structure belongs
    * @param begin offset to the begin spot in the SofA
-   * @param end offset to the end spot in the SofA 
-  */  
+   * @param end offset to the end spot in the SofA
+  */
   public DocumentMetadata(JCas jcas, int begin, int end) {
     super(jcas);
     setBegin(begin);
     setEnd(end);
     readObject();
-  }   
+  }
 
-  /** 
+  /**
    * <!-- begin-user-doc -->
    * Write your own initialization here
    * <!-- end-user-doc -->
    *
-   * @generated modifiable 
+   * @generated modifiable
    */
   private void readObject() {/*default - does nothing empty block */}
-     
- 
-    
+
+
+
   //*--------------*
   //* Feature: sourceUri
 
-  /** getter for sourceUri - gets 
+  /** getter for sourceUri - gets
    * @generated
-   * @return value of the feature 
+   * @return value of the feature
    */
   public String getSourceUri() {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_sourceUri == null)
       jcasType.jcas.throwFeatMissing("sourceUri", "com.textocat.textokit.commons.DocumentMetadata");
     return jcasType.ll_cas.ll_getStringValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_sourceUri);}
-    
-  /** setter for sourceUri - sets  
+
+  /** setter for sourceUri - sets
    * @generated
-   * @param v value to set into the feature 
+   * @param v value to set into the feature
    */
   public void setSourceUri(String v) {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_sourceUri == null)
       jcasType.jcas.throwFeatMissing("sourceUri", "com.textocat.textokit.commons.DocumentMetadata");
-    jcasType.ll_cas.ll_setStringValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_sourceUri, v);}    
-   
-    
+    jcasType.ll_cas.ll_setStringValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_sourceUri, v);}
+
+
   //*--------------*
   //* Feature: offsetInSource
 
-  /** getter for offsetInSource - gets 
+  /** getter for offsetInSource - gets
    * @generated
-   * @return value of the feature 
+   * @return value of the feature
    */
   public long getOffsetInSource() {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_offsetInSource == null)
       jcasType.jcas.throwFeatMissing("offsetInSource", "com.textocat.textokit.commons.DocumentMetadata");
     return jcasType.ll_cas.ll_getLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_offsetInSource);}
-    
-  /** setter for offsetInSource - sets  
+
+  /** setter for offsetInSource - sets
    * @generated
-   * @param v value to set into the feature 
+   * @param v value to set into the feature
    */
   public void setOffsetInSource(long v) {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_offsetInSource == null)
       jcasType.jcas.throwFeatMissing("offsetInSource", "com.textocat.textokit.commons.DocumentMetadata");
-    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_offsetInSource, v);}    
-   
-    
+    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_offsetInSource, v);}
+
+
   //*--------------*
   //* Feature: documentSize
 
-  /** getter for documentSize - gets 
+  /** getter for documentSize - gets
    * @generated
-   * @return value of the feature 
+   * @return value of the feature
    */
   public long getDocumentSize() {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_documentSize == null)
       jcasType.jcas.throwFeatMissing("documentSize", "com.textocat.textokit.commons.DocumentMetadata");
     return jcasType.ll_cas.ll_getLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_documentSize);}
-    
-  /** setter for documentSize - sets  
+
+  /** setter for documentSize - sets
    * @generated
-   * @param v value to set into the feature 
+   * @param v value to set into the feature
    */
   public void setDocumentSize(long v) {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_documentSize == null)
       jcasType.jcas.throwFeatMissing("documentSize", "com.textocat.textokit.commons.DocumentMetadata");
-    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_documentSize, v);}    
-   
-    
+    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_documentSize, v);}
+
+
   //*--------------*
   //* Feature: startProcessingTime
 
-  /** getter for startProcessingTime - gets 
+  /** getter for startProcessingTime - gets
    * @generated
-   * @return value of the feature 
+   * @return value of the feature
    */
   public long getStartProcessingTime() {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_startProcessingTime == null)
       jcasType.jcas.throwFeatMissing("startProcessingTime", "com.textocat.textokit.commons.DocumentMetadata");
     return jcasType.ll_cas.ll_getLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_startProcessingTime);}
-    
-  /** setter for startProcessingTime - sets  
+
+  /** setter for startProcessingTime - sets
    * @generated
-   * @param v value to set into the feature 
+   * @param v value to set into the feature
    */
   public void setStartProcessingTime(long v) {
     if (DocumentMetadata_Type.featOkTst && ((DocumentMetadata_Type)jcasType).casFeat_startProcessingTime == null)
       jcasType.jcas.throwFeatMissing("startProcessingTime", "com.textocat.textokit.commons.DocumentMetadata");
-    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_startProcessingTime, v);}    
+    jcasType.ll_cas.ll_setLongValue(addr, ((DocumentMetadata_Type)jcasType).casFeatCode_startProcessingTime, v);}
   }
 
     
--- Textokit.Commons/src/main/jcas/com/textocat/textokit/commons/DocumentMetadata_Type.java
+++ Textokit.Commons/src/main/jcas/com/textocat/textokit/commons/DocumentMetadata_Type.java
@@ -1,4 +1,20 @@
 
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 /* First created by JCasGen Wed Nov 11 19:41:34 MSK 2015 */
 package com.textocat.textokit.commons;
 
@@ -13,17 +29,17 @@ import org.apache.uima.cas.impl.FeatureImpl;
 import org.apache.uima.cas.Feature;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
-/** 
+/**
  * Updated by JCasGen Wed Nov 11 19:41:34 MSK 2015
  * @generated */
 public class DocumentMetadata_Type extends Annotation_Type {
-  /** @generated 
+  /** @generated
    * @return the generator for this type
    */
   @Override
   protected FSGenerator getFSGenerator() {return fsGenerator;}
   /** @generated */
-  private final FSGenerator fsGenerator = 
+  private final FSGenerator fsGenerator =
     new FSGenerator() {
       public FeatureStructure createFS(int addr, CASImpl cas) {
   			 if (DocumentMetadata_Type.this.useExistingInstance) {
@@ -41,19 +57,19 @@ public class DocumentMetadata_Type extends Annotation_Type {
   /** @generated */
   @SuppressWarnings ("hiding")
   public final static int typeIndexID = DocumentMetadata.typeIndexID;
-  /** @generated 
+  /** @generated
      @modifiable */
   @SuppressWarnings ("hiding")
   public final static boolean featOkTst = JCasRegistry.getFeatOkTst("com.textocat.textokit.commons.DocumentMetadata");
- 
+
   /** @generated */
   final Feature casFeat_sourceUri;
   /** @generated */
   final int     casFeatCode_sourceUri;
   /** @generated
    * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
+   * @return the feature value
+   */
   public String getSourceUri(int addr) {
         if (featOkTst && casFeat_sourceUri == null)
       jcas.throwFeatMissing("sourceUri", "com.textocat.textokit.commons.DocumentMetadata");
@@ -61,23 +77,23 @@ public class DocumentMetadata_Type extends Annotation_Type {
   }
   /** @generated
    * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
+   * @param v value to set
+   */
   public void setSourceUri(int addr, String v) {
         if (featOkTst && casFeat_sourceUri == null)
       jcas.throwFeatMissing("sourceUri", "com.textocat.textokit.commons.DocumentMetadata");
     ll_cas.ll_setStringValue(addr, casFeatCode_sourceUri, v);}
-    
-  
- 
+
+
+
   /** @generated */
   final Feature casFeat_offsetInSource;
   /** @generated */
   final int     casFeatCode_offsetInSource;
   /** @generated
    * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
+   * @return the feature value
+   */
   public long getOffsetInSource(int addr) {
         if (featOkTst && casFeat_offsetInSource == null)
       jcas.throwFeatMissing("offsetInSource", "com.textocat.textokit.commons.DocumentMetadata");
@@ -85,23 +101,23 @@ public class DocumentMetadata_Type extends Annotation_Type {
   }
   /** @generated
    * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
+   * @param v value to set
+   */
   public void setOffsetInSource(int addr, long v) {
         if (featOkTst && casFeat_offsetInSource == null)
       jcas.throwFeatMissing("offsetInSource", "com.textocat.textokit.commons.DocumentMetadata");
     ll_cas.ll_setLongValue(addr, casFeatCode_offsetInSource, v);}
-    
-  
- 
+
+
+
   /** @generated */
   final Feature casFeat_documentSize;
   /** @generated */
   final int     casFeatCode_documentSize;
   /** @generated
    * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
+   * @return the feature value
+   */
   public long getDocumentSize(int addr) {
         if (featOkTst && casFeat_documentSize == null)
       jcas.throwFeatMissing("documentSize", "com.textocat.textokit.commons.DocumentMetadata");
@@ -109,23 +125,23 @@ public class DocumentMetadata_Type extends Annotation_Type {
   }
   /** @generated
    * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
+   * @param v value to set
+   */
   public void setDocumentSize(int addr, long v) {
         if (featOkTst && casFeat_documentSize == null)
       jcas.throwFeatMissing("documentSize", "com.textocat.textokit.commons.DocumentMetadata");
     ll_cas.ll_setLongValue(addr, casFeatCode_documentSize, v);}
-    
-  
- 
+
+
+
   /** @generated */
   final Feature casFeat_startProcessingTime;
   /** @generated */
   final int     casFeatCode_startProcessingTime;
   /** @generated
    * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
+   * @return the feature value
+   */
   public long getStartProcessingTime(int addr) {
         if (featOkTst && casFeat_startProcessingTime == null)
       jcas.throwFeatMissing("startProcessingTime", "com.textocat.textokit.commons.DocumentMetadata");
@@ -133,39 +149,39 @@ public class DocumentMetadata_Type extends Annotation_Type {
   }
   /** @generated
    * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
+   * @param v value to set
+   */
   public void setStartProcessingTime(int addr, long v) {
         if (featOkTst && casFeat_startProcessingTime == null)
       jcas.throwFeatMissing("startProcessingTime", "com.textocat.textokit.commons.DocumentMetadata");
     ll_cas.ll_setLongValue(addr, casFeatCode_startProcessingTime, v);}
-    
-  
+
+
 
 
 
   /** initialize variables to correspond with Cas Type and Features
 	 * @generated
 	 * @param jcas JCas
-	 * @param casType Type 
+	 * @param casType Type
 	 */
   public DocumentMetadata_Type(JCas jcas, Type casType) {
     super(jcas, casType);
     casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
 
- 
+
     casFeat_sourceUri = jcas.getRequiredFeatureDE(casType, "sourceUri", "uima.cas.String", featOkTst);
     casFeatCode_sourceUri  = (null == casFeat_sourceUri) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_sourceUri).getCode();
 
- 
+
     casFeat_offsetInSource = jcas.getRequiredFeatureDE(casType, "offsetInSource", "uima.cas.Long", featOkTst);
     casFeatCode_offsetInSource  = (null == casFeat_offsetInSource) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_offsetInSource).getCode();
 
- 
+
     casFeat_documentSize = jcas.getRequiredFeatureDE(casType, "documentSize", "uima.cas.Long", featOkTst);
     casFeatCode_documentSize  = (null == casFeat_documentSize) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_documentSize).getCode();
 
- 
+
     casFeat_startProcessingTime = jcas.getRequiredFeatureDE(casType, "startProcessingTime", "uima.cas.Long", featOkTst);
     casFeatCode_startProcessingTime  = (null == casFeat_startProcessingTime) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_startProcessingTime).getCode();
 
