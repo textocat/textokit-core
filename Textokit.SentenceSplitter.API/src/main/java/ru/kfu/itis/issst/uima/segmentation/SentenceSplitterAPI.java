0c61604421133d5633dceb159c510c953af2bf77
Rinat Gareev <rinat@textocat.com>
2015-11-12 16:55:14 +0300

    moved SentenceSplitter.* modules

---
+++ Textokit.SentenceSplitter.API/src/main/java/ru/kfu/itis/issst/uima/segmentation/SentenceSplitterAPI.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package ru.kfu.itis.issst.uima.segmentation;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.metadata.Import;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.resource.metadata.impl.Import_impl;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+
+import ru.kfu.cll.uima.segmentation.fstype.Sentence;
+import ru.kfu.itis.cll.uima.util.PipelineDescriptorUtils;
+import ru.kfu.itis.issst.uima.tokenizer.TokenizerAPI;
+
+/**
+ * A class that provides constants and methods to use a sentence-splitter.
+ * <p>
+ * By default an implementation of sentence splitter requires an input CAS with
+ * token annotations. See {@link TokenizerAPI}.
+ * <p>
+ * Sentence splitter enriches a CAS with {@link Sentence} annotations.
+ * 
+ * @author Rinat Gareev
+ * 
+ */
+public class SentenceSplitterAPI {
+
+	/**
+	 * A name of analysis engine description that can be imported. An
+	 * implementation of sentence-splitter should provide its description at
+	 * this location either in classpath or UIMA datapath.
+	 */
+	public static final String AE_SENTENCE_SPLITTER = "ru.kfu.itis.issst.uima.segmentation.sentence-splitter-ae";
+
+	/**
+	 * A name of type-system description that define types produced by
+	 * implementations of sentence splitter.
+	 */
+	public static final String TYPESYSTEM_SENTENCES = "ru.kfu.itis.issst.uima.segmentation.segmentation-TypeSystem";
+
+	/**
+	 * @return type-system description instance
+	 */
+	public static TypeSystemDescription getTypeSystemDescription() {
+		return TypeSystemDescriptionFactory.createTypeSystemDescription(TYPESYSTEM_SENTENCES);
+	}
+
+	/**
+	 * @return AE description instance
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
+	public static AnalysisEngineDescription getAEDescription() throws UIMAException, IOException {
+		return AnalysisEngineFactory.createEngineDescription(AE_SENTENCE_SPLITTER);
+	}
+
+	/**
+	 * @return import instance. This is preferred way to include the AE into
+	 *         pipeline, especially when a pipeline descriptor is expected to be
+	 *         serialized into XML.
+	 * @see PipelineDescriptorUtils#createAggregateDescription(java.util.List,
+	 *      java.util.List)
+	 */
+	public static Import getAEImport() {
+		Import result = new Import_impl();
+		result.setName(AE_SENTENCE_SPLITTER);
+		return result;
+	}
+
+	private SentenceSplitterAPI() {
+	}
+
+}
---
+++ Textokit.SentenceSplitter.Heuristic/src/test/java/ru/kfu/itis/issst/uima/segmentation/GenerateSentenceSplitterDescriptor.java
@@ -0,0 +1,33 @@
+package ru.kfu.itis.issst.uima.segmentation;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Rinat Gareev
+ * 
+ */
+public class GenerateSentenceSplitterDescriptor {
+
+	public static void main(String[] args) throws UIMAException, IOException, SAXException {
+		String outputPath = "src/main/resources/ru/kfu/itis/issst/uima/segmentation/sentence-splitter-ae.xml";
+		TypeSystemDescription tsDesc = SentenceSplitterAPI.getTypeSystemDescription();
+		AnalysisEngineDescription desc = createEngineDescription(SentenceSplitter.class, tsDesc);
+		FileOutputStream out = FileUtils.openOutputStream(new File(outputPath));
+		try {
+			desc.toXML(out);
+		} finally {
+			IOUtils.closeQuietly(out);
+		}
+	}
+}
