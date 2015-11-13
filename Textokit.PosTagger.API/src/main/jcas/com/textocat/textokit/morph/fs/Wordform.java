e0d797d8ee2d84b344396dfa01c79ef8055213fb
Rinat Gareev <rinat@textocat.com>
2015-11-13 20:09:14 +0300

    added copyright notice for sources in Morph.PosTagger.*

--- Textokit.PosTagger.API/src/main/jcas/com/textocat/textokit/morph/fs/Wordform.java
+++ Textokit.PosTagger.API/src/main/jcas/com/textocat/textokit/morph/fs/Wordform.java
@@ -1,5 +1,21 @@
 
 
+/*
+ *    Copyright 2015 Textocat
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 /* First created by JCasGen Fri Nov 13 19:45:22 MSK 2015 */
 package com.textocat.textokit.morph.fs;
 
@@ -128,7 +144,7 @@ public class Wordform extends TOP {
   public void setPosBits(int i, long v) { 
     if (Wordform_Type.featOkTst && ((Wordform_Type)jcasType).casFeat_posBits == null)
       jcasType.jcas.throwFeatMissing("posBits", "com.textocat.textokit.morph.fs.Wordform");
-    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type)jcasType).casFeatCode_posBits), i);
+    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type) jcasType).casFeatCode_posBits), i);
     jcasType.ll_cas.ll_setLongArrayValue(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type)jcasType).casFeatCode_posBits), i, v);}
    
     
@@ -216,7 +232,7 @@ public class Wordform extends TOP {
   public void setGrammems(int i, String v) { 
     if (Wordform_Type.featOkTst && ((Wordform_Type)jcasType).casFeat_grammems == null)
       jcasType.jcas.throwFeatMissing("grammems", "com.textocat.textokit.morph.fs.Wordform");
-    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type)jcasType).casFeatCode_grammems), i);
+    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type) jcasType).casFeatCode_grammems), i);
     jcasType.ll_cas.ll_setStringArrayValue(jcasType.ll_cas.ll_getRefValue(addr, ((Wordform_Type)jcasType).casFeatCode_grammems), i, v);}
    
     
