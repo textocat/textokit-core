32ce0231fcb0fab60811b6721e88b040ac27b74e
Rinat Gareev <rinat@textocat.com>
2015-11-15 00:32:11 +0300

    changed compile-opencorpora-dictionary script to use XmlDictionaryPSP

--- Textokit.PosTagger.Util.RNC/compile-opencorpora-dictionary.sh
+++ Textokit.PosTagger.Util.RNC/compile-opencorpora-dictionary.sh
@@ -3,7 +3,7 @@ if [[ -z $1 || -z $2  ]] ; then
   echo "Usage: <opencorpora-dict-xml> <output-file>"
   exit 1
 fi
-java_app_arguments="--dict-extension-class,com.textocat.textokit.morph.ruscorpora.RNCDictionaryExtension"
+java_app_arguments="--dict-extension-class,com.textocat.textokit.morph.ruscorpora.RNCDictionaryExtension,--output-variant,rnc"
 java_app_arguments="$java_app_arguments,-i,$1,-o,$2"
-mvn exec:java -Pwith-logging-impl -Dexec.mainClass=com.textocat.textokit.morph.opencorpora.resource.XmlDictionaryParserLauncher \
+mvn exec:java -Pwith-logging-impl -Dexec.mainClass=com.textocat.textokit.morph.opencorpora.resource.XmlDictionaryPSP \
   -Dexec.arguments="$java_app_arguments"
